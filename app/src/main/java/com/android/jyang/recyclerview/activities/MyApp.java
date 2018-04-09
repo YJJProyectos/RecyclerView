9f6f847c2865ec44fbc889274cb37abc0763aea3
Jia Jun Yang <jyang@baufest.com>
2018-04-09 15:45:35 -0300

    Splash Screen inicio

---
+++ app/src/main/java/com/android/jyang/recyclerview/activities/MyApp.java
@@ -0,0 +1,19 @@
+package com.android.jyang.recyclerview.activities;
+
+import android.app.Application;
+import android.os.SystemClock;
+
+import java.time.Clock;
+
+/**
+ * Created by jyang on 9/4/2018.
+ */
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SystemClock.sleep(2000);
+    }
+}
